3b5135ffb7a0e2597a3faa3d3728812e60ce99ac
Olve Maudal <olve.maudal@gmail.com>
2022-02-28 21:57:03 +0000

    Implement read_lasfile()

--- mylastool.py
+++ mylastool.py
@@ -9,9 +9,22 @@ def print_directory(container):
     for blob in container.list_blobs():
         print(f'{blob.size:>20} {blob.name}')
 
+def read_lasfile(container, filename):
+    blob_client = container.get_blob_client(filename)
+    data = blob_client.download_blob().content_as_bytes()
+    lines = []
+    for line in data.splitlines():
+        lines.append(line.decode("ascii", errors='ignore'))
+    return lines
+
 def main():
     container = get_container()
-    print_directory(container)
+    #print_directory(container)
+
+    lasfile = '31_5-7 Eos/07.Borehole_Seismic/TZV_TIME_SYNSEIS_2020-01-17_2.LAS'
+    lines = read_lasfile(container, lasfile)
+    for line in lines:
+        print(line)
 
 if __name__ == '__main__':
     main()
