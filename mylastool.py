312bd5f124a97bae4586f65172b47114af103003
Olve Maudal <olve.maudal@gmail.com>
2022-03-01 16:47:28 +0000

    Implement print header/data section

--- mylastool.py
+++ mylastool.py
@@ -24,13 +24,31 @@ def read_lasfile(container, filename):
         lines.append(line.decode("ascii", errors='ignore'))
     return lines
 
+def find_section_index(lines, prefix):
+    idx = 0
+    for line in lines:
+        if line.startswith(prefix):
+            break
+        idx += 1
+    return idx
+
+def print_header_section(lines):
+    for line in lines[:find_section_index(lines, '~A')]:
+        print(line)
+
+def print_data_section(lines):
+    for line in lines[find_section_index(lines, '~A')+1:]:
+        print(line)
+
 def main():
     container = get_container()
     #print_list_of_lasfiles(container)
     filename = "31_5-7 Eos/07.Borehole_Seismic/TZV_TIME_SYNSEIS_2020-01-17_2.LAS"
     lines = read_lasfile(container, filename)
-    for line in lines:
-        print(line)
+    print_header_section(lines)
+    #print_data_section(lines)
+    #for line in lines:
+    #    print(line)
 
 if __name__ == '__main__':
     main()
